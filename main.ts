1b68b216837ea785f7ec9e2f7f825fec36c3d088
Johan <3921204+johan-mattias@users.noreply.github.com>
2021-01-14 10:43:23 +0100

    removed some comments

--- main.ts
+++ main.ts
@@ -41,9 +41,6 @@ function Setup(){
     playerPosition = 0;
 
     openSlots = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    /*for (let i = 0; i <= 8; i++){
-        openSlots.push(i);
-    }*/
 
     slotWeights = defaultWeights;
 
